0fc33fceb002f80121329fa96036962a5c9ed5f3
pmh <hieu.pm2029@gmail.com>
2022-06-27 01:21:21 +0700

    Use Firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBV9o-0wZrU6CjU5kdxuvYx9A3K9I3oqzk",
+  authDomain: "c-estsibon-5e87f.firebaseapp.com",
+  projectId: "c-estsibon-5e87f",
+  storageBucket: "c-estsibon-5e87f.appspot.com",
+  messagingSenderId: "574310287729",
+  appId: "1:574310287729:web:88d8d5680acd090f8b8ed0"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
