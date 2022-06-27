32a671cd1ab5ab611b85f0d9459fa04923a19338
pmh <hieu.pm2029@gmail.com>
2022-06-27 10:00:57 +0700

    Login by Authentication

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -12,28 +12,26 @@ const firebaseConfig = {
   projectId: "c-estsibon-5e87f",
   storageBucket: "c-estsibon-5e87f.appspot.com",
   messagingSenderId: "574310287729",
-  appId: "1:574310287729:web:88d8d5680acd090f8b8ed0"
+  appId: "1:574310287729:web:88d8d5680acd090f8b8ed0",
 };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 const db = firebase.firestore();
+export const auth = firebase.auth();
+export default firebase;
 
 export const getFirebaseItems = async () => {
   try {
-    const snapshot = await db
-      .collection("todos")
-      .get();
-    const items = snapshot.docs.map(
-      (doc) => ({ ...doc.data(), id: doc.id })
-    );
+    const snapshot = await db.collection("todos").get();
+    const items = snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
     return items;
   } catch (err) {
     console.log(err);
     return [];
   }
-}
+};
 
 export const addFirebaseItem = async (item) => {
   try {
@@ -42,7 +40,7 @@ export const addFirebaseItem = async (item) => {
   } catch (err) {
     console.log(err);
   }
-}
+};
 
 export const updateFirebaseItem = async (item, id) => {
   try {
@@ -51,12 +49,37 @@ export const updateFirebaseItem = async (item, id) => {
   } catch (err) {
     console.log(err);
   }
-}
+};
 
 export const clearFirebaseItem = async (item) => {
   const todoRef = db.collection("todos").doc(item.id);
-  await todoRef.delete().then(function () {
-  }).catch(function (err) {
-    console.log(err);
-  });
-}; 
+  await todoRef
+    .delete()
+    .then(function () {})
+    .catch(function (err) {
+      console.log(err);
+    });
+};
+
+export const uiConfig = {
+  signInFlow: "popup",
+  signInSuccessUrl: "/",
+  signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+};
+
+export const storeUserInfo = async (user) => {
+  const { uid } = user;
+  const userDoc = await db.collection("users").doc(uid).get();
+  if (!userDoc.exists) {
+    await db.collection("users").doc(uid).set({ name: user.displayName });
+    return {
+      name: user.displayName,
+      id: uid,
+    };
+  } else {
+    return {
+      id: uid,
+      ...userDoc.data(),
+    };
+  }
+};
